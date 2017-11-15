5897b84d821040e2c5577260081150785b968d77
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-11-14 17:57:56 -0800

    began initial express app setup

--- app.js
+++ app.js
@@ -1,2 +1,74 @@
 "use strict";
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const multer = require('multer');
+const flash = require('connect-flash');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const db = mongoose.connection;
 
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const CONFIG = path.join(__dirname, 'config');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// file uploads setup
+app.use(multer({
+  dest: 'uploads/'
+}));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// sessions setup
+app.use(session(
+  require(path.join(CONFIG, 'session'))
+));
+
+// passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+
+// validator setup
+// to be added
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
