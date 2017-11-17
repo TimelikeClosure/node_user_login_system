3c64156e99b35742d88c66ec794f6905f8832d81
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-11-16 18:44:20 -0800

    started adding registration form handling

--- includes/validator.js
+++ includes/validator.js
@@ -1,10 +1,27 @@
 "use strict";
-const { check, validationResult } = require('express-validator/check');
-const { matchedData, sanitize } = require('express-validator/filter');
+const { check, body, validationResult } = require('express-validator/check');
+const { matchedData, sanitize, sanitizeBody } = require('express-validator/filter');
+
+const bodyFieldsExist = fields => fields.map(field => (
+    body(field, `No ${field} provided`).exists()
+));
+
+const bodyFieldsTrim = fields => fields.map(field => (
+    sanitizeBody(field).trim()
+));
+
+const bodyFieldsNonEmpty = fields => fields.map(field => (
+    body(field, `No ${field} provided`).not().isEmpty()
+));
 
 module.exports = {
     check,
+    body,
     validationResult,
     matchedData,
-    sanitize
+    sanitize,
+    sanitizeBody,
+    bodyFieldsExist,
+    bodyFieldsTrim,
+    bodyFieldsNonEmpty
 };
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@
 const { requirePath, PATHS } = require('../paths');
 const express = require('express');
 const router = express.Router();
+const upload = requirePath(PATHS.includes, 'upload');
+const { validationResult, bodyFieldsExist, bodyFieldsTrim, bodyFieldsNonEmpty } = requirePath(PATHS.includes, 'validator');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,6 +12,38 @@ router.get('/', function(req, res, next) {
 
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register' });
+});
+
+router.post('/register', upload.single('profile_image'), bodyFieldsExist([
+  'name', 'email', 'username', 'password', 'password_confirm'
+]), bodyFieldsTrim([
+  'name', 'email', 'username'
+]), bodyFieldsNonEmpty([
+  'name', 'email', 'username', 'password'
+]), [
+  /* body('password_confirm', 'both passwords must match')
+    .equals(matchedData(req, {locations: ['body']}).password) */
+], function(req, res, next) {
+  const {name, email, username, password, password_confirm} = req.body;
+  if (req.file){
+    console.log('Uploading file');
+    var profileImage = req.file.filename;
+  } else {
+    console.log('No file uploaded');
+    var profileImage = 'noimage.jpg';
+  }
+
+  // Form Validation
+
+
+  // Check Errors
+  const errors = validationResult(req);
+  if(errors.isEmpty()){
+    console.log('there were no registration form errors');
+  } else {
+    console.log('the following registration form errors occured:');
+    console.log(JSON.stringify(errors.array()));
+  }
 });
 
 router.get('/login', function(req, res, next) {
