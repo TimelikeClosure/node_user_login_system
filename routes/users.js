a21ce898c7982a6abbb8e5dee3a62d7153e698ca
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-11-21 11:30:41 -0800

    cleaned up user registration validation middleware

--- routes/users.js
+++ routes/users.js
@@ -14,40 +14,36 @@ router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register' });
 });
 
-router.post('/register', upload.single('profile_image'), [
-  sanitizeBody('name').stripLow().trim(),
-  createValidationChain('name').nonEmpty().isName()(body)
-], [
-  sanitizeBody('email').stripLow().trim(),
-  createValidationChain('email').nonEmpty().isEmail()(body)
-], [
-  sanitizeBody('username').stripLow().trim(),
-  createValidationChain('username').nonEmpty().isUsername()(body)
-], [
-  sanitizeBody('password').stripLow(),
-  createValidationChain('password').nonEmpty().isPassword()(body)
-], [
-  sanitizeBody('password_confirm').stripLow(),
-  createValidationChain('password_confirm').nonEmpty().matchesField('password', 'body')(body)
-], function(req, res, next) {
-  const {name, email, username, password, password_confirm} = req.body;
-  if (req.file){
-    console.log('Uploading file');
-    var profileImage = req.file.filename;
-  } else {
-    console.log('No file uploaded');
-    var profileImage = 'noimage.jpg';
-  }
+router.post('/register', upload.single('profile_image'),
+  ['name', 'email', 'username', 'password', 'password_confirm']
+    .map(field => sanitizeBody(field).stripLow()),
+  ['name', 'email', 'username']
+    .map(field => sanitizeBody(field).trim()),
+  createValidationChain('name').nonEmpty().isName()(body),
+  createValidationChain('email').nonEmpty().isEmail()(body),
+  createValidationChain('username').nonEmpty().isUsername()(body),
+  createValidationChain('password').nonEmpty().isPassword()(body),
+  createValidationChain('password_confirm').nonEmpty().matchesField('password', 'body')(body),
+  function(req, res, next) {
+    const {name, email, username, password, password_confirm} = req.body;
+    if (req.file){
+      console.log('Uploading file');
+      var profileImage = req.file.filename;
+    } else {
+      console.log('No file uploaded');
+      var profileImage = 'noimage.jpg';
+    }
 
-  // Check Errors
-  const errors = validationResult(req);
-  if(errors.isEmpty()){
-    console.log('there were no registration form errors');
-  } else {
-    console.log('the following registration form errors occured:');
-    console.log(errors.array().map(value => JSON.stringify(value)).join('\n'));
+    // Check Errors
+    const errors = validationResult(req);
+    if(errors.isEmpty()){
+      console.log('there were no registration form errors');
+    } else {
+      console.log('the following registration form errors occured:');
+      console.log(errors.array().map(value => JSON.stringify(value)).join('\n'));
+    }
   }
-});
+);
 
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
